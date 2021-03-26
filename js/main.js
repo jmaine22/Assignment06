c3480d6d9465cbadf8dc7c180a57cad48edacb07
Jmaine22 <jmaine22@gmail.com>
2021-03-25 19:47:44 -0700

    Assignment06

--- js/main.js
+++ js/main.js
@@ -3,10 +3,11 @@ window.addEventListener('load', init);
 // HANDLE THE CLICK EVENT FOR THE BTNADDEMPLOYEE BUTTON
 function init(){
     let btn = document.getElementById('btnAddEmployee');
+    let myWindow;
     btn.addEventListener('click', function () {
         console.log('Hello World');
-        window.open('add-employee.html');
-        window.resizeTo(250, 250);
+        myWindow = window.open('add-employee.html', 'width=100, height=100');
+        myWindow.resizeTo(250, 250);
     });
 }
 // OPEN THE ADD-EMPLOYEE.HTML PAGE WITHIN A POPUP
