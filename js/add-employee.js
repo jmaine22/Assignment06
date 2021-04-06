cc5d7e2403bd231e608959ab7fba6ecfa6115f88
Jmaine22 <jmaine22@gmail.com>
2021-04-05 22:52:40 -0700

    Assigment06

--- js/add-employee.js
+++ js/add-employee.js
@@ -1,5 +1,4 @@
-// HANDLE THE LOAD EVENT OF THE WINDOW
-window.addEventListener('load', init);
+
 // CREATE A HELPER FUNCTION TO RETRIEVE THE HTML ELEMENTS FROM THE DOM
 function init() {
     "use strict";
@@ -16,28 +15,30 @@ function init() {
     document.getElementById('extension');
     document.getElementById('email');
     document.getElementById('department');
-}
-// HANDLE THE SUBMISSION OF THE FORM AND THEN IMMEDIATELY PREVENT THE SUBMISSION
-document.addEventListener('DOMContentLoaded', () => {
-    let mySubmit = document.getElementById('submit');
-    mySubmit.addEventListener('click', (e) => {
-        e.preventDefault();
-    })
-});
-document.addEventListener('DOMContentLoaded', () => {
-    let myCancel = document.getElementById('cancel');
-    myCancel.addEventListener('click', (e) => {
+
+    // HANDLE THE SUBMISSION OF THE FORM AND THEN IMMEDIATELY PREVENT THE SUBMISSION
+    document.addEventListener('DOMContentLoaded', () => {
+        let mySubmit = document.getElementById('submit');
+        mySubmit.addEventListener('click', (e) => {
+            e.preventDefault();
+        })
+    });
+    document.addEventListener('DOMContentLoaded', () => {
+        let myCancel = document.getElementById('cancel');
+        myCancel.addEventListener('click', (e) => {
         window.close();
-    })
-});
-// CREATE 5 VARIABLES FOR ID, NAME, EXT, EMAIL, AND DEPT
+        })
+    });
+    // CREATE 5 VARIABLES FOR ID, NAME, EXT, EMAIL, AND DEPT
 let myID;
 let myName;
 let myExtension;
 let myEmail;
 let myDepartment;
 // SET THOSE VARIABLES TO WHATEVER THE USER ENTERS INTO THE FORM ELEMENTS
-let employee = document.addEventListener('click', () => {
+
+let employee = window.document.getElementById("submit");
+employee.addEventListener("click", function() {
     myID = document.getElementById('id').value;
     console.log(myID);
     myName = document.getElementById('name').value;
@@ -48,16 +49,19 @@ let employee = document.addEventListener('click', () => {
     console.log(myEmail);
     myDepartment = document.getElementById('department').value;
     console.log(myDepartment);
-});
-// GET THE LOGINDETAILS OUTPUT ELEMENT FROM THE PARENT PAGE
-let page = window.open('','loginDetail');
+    //page.opener.document.write('<p>Parent Window</p>');
+    // GET THE LOGINDETAILS OUTPUT ELEMENT FROM THE PARENT PAGE
+    let page = window.opener.document.getElementById('loginDetails');
+    console.log(page);
+    page.innerHTML = "<br><br>8-Digit Employee ID" + myID + "<br>Full Name" + myName + "<br>4-Digit Extension" + myExtension + "<br>Email" + myEmail + "<br>Department" + myDepartment;
+})
+    
 //let loginDetail = document.getElementById('loginDetails');
 // SET THE TEXT OF THE LOGINDETAILS ELEMENT TO THE ABOVE SET VARIABLES
-document.addEventListener('submit', () => {
-    //page.opener.document.write('<p>Parent Window</p>');
-    console.log(page);
-})
 
 // CLOSE THE POPUP
 
 // THE DATA SHOULD SHOW ON THE INDEX.HTML PAGE 
+}
+// HANDLE THE LOAD EVENT OF THE WINDOW
+window.addEventListener('load', init);
